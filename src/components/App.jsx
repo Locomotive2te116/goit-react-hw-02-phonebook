ba94bb4c8bc2ea8778f0652aec05a2506fe56e0a
Locomotive2te116 <locomotive2te116@gmail.com>
2023-12-07 11:00:17 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    const { contacts, name } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <h2>Name</h2>
+        <input type="text" name={name} required></input>
+        <button>Add contact</button>
+        React homework template
+      </div>
+    );
+  }
+}
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
